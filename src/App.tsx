b3362065b129665db750540c9d18ea16b0e8de1c
macycle <1124942097@qq.com>
2020-09-27 14:08:18 +0800

    sb ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
-import styled from 'styled-components'
+import './reset.scss'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav';
 
-const Button=styled.button`color:red`;
+
+const Wrapper=styled.div`
+    display:flex;
+    flex-direction:column;
+    height:100vh;
+    
+`
+
+const Main=styled.div`
+    flex-grow:1;
+`
 
 function App() {
   return (
-    <div className="App">
-      <div>hello world</div>
-      <Button>按钮</Button>
-    </div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tag">
+              <Tag />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistic">
+              <Statistic />
+            </Route>
+            <Redirect exact from='/' to='/money'></Redirect>
+            <Route path="*">
+              <Nomatch />
+            </Route>
+          </Switch>
+        </Main>
+       <Nav />
+      </Wrapper>
+    </Router>
   );
 }
+
+function Tag() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistic() {
+  return <h2>统计页</h2>;
+}
+
+function Nomatch(){
+  return <h2>找不到该网页</h2>;
+}
+
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import React from 'react';
+import {Link} from 'react-router-dom';
+const NavWrapper=styled.nav`
+   border:1px solid yellow;
+   line-height:24px;
+   box-shadow:0 0 3px rgba(0,0,0,0.25);
+   > ul{
+     display:flex;
+     > li{
+       flex:1;
+       text-align:center;
+       padding:16px 0
+     }
+   }
+`
+
+function Nav(){
+    return (
+        <NavWrapper>
+            <ul>
+                <li>
+                    <Link to="/tag">标签</Link>
+                </li>
+                <li>
+                    <Link to="/money">记账</Link>
+                </li>
+                <li>
+                    <Link to="/statistic">统计</Link>
+                </li>
+            </ul>
+        </NavWrapper>
+    )
+}
+
+
+export default Nav;
