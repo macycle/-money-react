5da8b3d5613a2d33c7890f402dbe08175c95743f
macycle <1124942097@qq.com>
2020-09-27 17:47:09 +0800

    使用了雪碧图进行icon的处理

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,28 @@
 import styled from 'styled-components';
 import React from 'react';
 import {Link} from 'react-router-dom';
+require('../icons/money.svg');
+require('../icons/tag.svg');
+require('../icons/statistic.svg');
+
+
+
 const NavWrapper=styled.nav`
-   border:1px solid yellow;
    line-height:24px;
    box-shadow:0 0 3px rgba(0,0,0,0.25);
    > ul{
      display:flex;
+     justify-content:center;
+     
      > li{
        flex:1;
-       text-align:center;
-       padding:16px 0
+       display:flex;
+       flex-direction:column;
+       align-items:center;
+       >.icon{
+            width:28px;
+            height: 28px;
+        }
      }
    }
 `;
@@ -20,12 +32,21 @@ const Nav=()=>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag"/>
+                    </svg>
                     <Link to="/tag">标签</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money"/>
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#statistic"/>
+                    </svg>
                     <Link to="/statistic">统计</Link>
                 </li>
             </ul>
